310a5e6cd263079c7f9b1740292ffc7643d520d1
Nischala Matcha <matchanischala.qa@gmail.com>
2025-06-17 11:45:09 +0530

    Completed the calculator app

--- SimpleProjects/calculator.js
+++ SimpleProjects/calculator.js
@@ -1,6 +1,15 @@
 let num1= document.getElementById("num1");
 let num2 = document.getElementById("num2");
 let result = document.getElementById("result");
+let form = document.getElementById("calculator");
+let calculatebtn = document.getElementById("operation");
+
+// Add event listeners to the calculate button
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the forgim from submitting and refreshing the page
+    let operation = calculatebtn.value; // Get the value of the clicked button
+    calculate(operation); // Call the calculate function with the operation
+});
 
 function calculate(operation) {
     let value1 = parseFloat(num1.value);
@@ -29,7 +38,7 @@ function calculate(operation) {
             return;
     }
 
-    result.innerHTML = "Result: " + output;
+    result.innerHTML = output;
 }
 
 // function add() {
